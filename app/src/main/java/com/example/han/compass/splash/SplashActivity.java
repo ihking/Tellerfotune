9341ee66da30dad98b4b7a654e6c0722070811f7
HAN <hsh8616@naver.com>
2016-08-21 07:15:44 +0900

    스플래시 푸시

--- app/src/main/java/com/example/han/compass/splash/SplashActivity.java
+++ app/src/main/java/com/example/han/compass/splash/SplashActivity.java
@@ -1,16 +1,23 @@
 package com.example.han.compass.splash;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
-
 import com.example.han.compass.MainActivity;
 import com.example.han.compass.R;
+import android.view.View;
+import android.widget.Button;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
 
-
+//import com.example.han.compass.library.GifImageView;
 
 public class SplashActivity extends AppCompatActivity {
+//    private GifImageView gifImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +25,10 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         Handler hd = new Handler();
-        hd.postDelayed(new splashhandler() , 2000);
+        hd.postDelayed(new splashhandler(), 2000);
+
+        //GifImageView gifView = (GifImageView) findViewById(R.id.gifImageView);
+        //gifView.setBytes(bitmapData);
     }
 
     private class splashhandler implements Runnable{
@@ -27,5 +37,24 @@ public class SplashActivity extends AppCompatActivity {
             SplashActivity.this.finish();
         }
     }
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        gifView.startAnimation();
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        gifView.stopAnimation();
+//    }
+
+//    gifImageView.setOnFrameAvailable(new GifImageView.OnFrameAvailable() {
+//        @Override
+//        public Bitmap onFrameAvailable(Bitmap bitmap) {
+//            return blurFilter.blur(bitmap);
+//        }
+//    });
 
 }
