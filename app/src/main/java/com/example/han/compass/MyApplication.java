bcaadf1d3ba5aba48b93e3e84761587ab4bc3afb
Lee Sang Woo <lol91@hanmail.net>
2016-08-21 07:04:12 +0900

    xml 부분수정

---
+++ app/src/main/java/com/example/han/compass/MyApplication.java
@@ -0,0 +1,13 @@
+package com.example.han.compass;
+
+import android.app.Application;
+
+/**
+ * Created by home on 2016-08-21.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/com/example/han/compass/splash/SplashActivity.java
+++ app/src/main/java/com/example/han/compass/splash/SplashActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 import com.example.han.compass.MainActivity;
 import com.example.han.compass.R;
 
@@ -17,8 +20,12 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(imageView);
+        Glide.with(this).load(R.mipmap.splash).crossFade().into(imageViewTarget);
+
         Handler hd = new Handler();
-        hd.postDelayed(new splashhandler() , 2000);
+        hd.postDelayed(new splashhandler() , 17000);
     }
 
     private class splashhandler implements Runnable{
